4dfd0e655d8450238639306156ed1b2eb5fb9600
Ebrahim573 <101477670+Ebrahim573@users.noreply.github.com>
2022-06-29 16:07:50 +0530

    Update AuthorModel.js

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true, useUnifiedTopology: true});
 const Schema = mongoose.Schema;
-
+mongoose.set("useFindAndModify", false);// part 2 point 9
 const AuthorSchema = new Schema({
     title : String,
     image: String,
