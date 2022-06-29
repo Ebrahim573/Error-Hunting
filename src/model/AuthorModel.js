f9a7e182b83396ce9ece0c179c401d895332f6f0
Ebrahim573 <101477670+Ebrahim573@users.noreply.github.com>
2022-06-30 00:00:26 +0530

    Update AuthorModel.js

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,23 +1,8 @@
 const mongoose = require('mongoose');
-mongoose
-	.connect(
-		'mongodb+srv://bharath:Livethegame14@firstclustertest.jgypz.mongodb.net/test?retryWrites=true&w=majority',
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true, //*9deprcated error fix for update operations
-			useFindAndModify: false
-		}
-	)
-	.then((x) => {
-		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-	})
-	.catch((err) => {
-		console.error('Error connecting to mongo', err);
-	});
 
-//mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Library',{useNewUrlParser: true, useUnifiedTopology: true});
 const Schema = mongoose.Schema;
-//mongoose.set("useFindAndModify", false);// part 2 point 9
+mongoose.set("useFindAndModify", false);// part 2 point 9
 const AuthorSchema = new Schema({
     title : String,
     image: String,
